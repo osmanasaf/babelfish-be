37de8c85dec5f8d03b9c9c4052e888360d348a63
Osman Asaf <osmaan.tatar@gmail.com>
2023-05-24 11:36:13 +0300

    init

---
+++ babelfish/src/main/java/com/codefirst/babelfish/BabelfishApplication.java
@@ -0,0 +1,13 @@
+package com.codefirst.babelfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelfishApplication.class, args);
+	}
+
+}
