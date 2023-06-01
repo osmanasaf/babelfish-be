2edafb491382a8906595fa6363ee9a2a4183d85c
Osman Asaf <osmaan.tatar@gmail.com>
2023-06-01 12:44:18 +0300

    project and customer dto added

---
+++ src/main/java/com/codefirst/babelfish/dto/CustomerDto.java
@@ -0,0 +1,7 @@
+package com.codefirst.babelfish.dto;
+
+public class CustomerDto {
+
+    private String companyName;
+    private String paymentTerm;
+}
---
+++ src/main/java/com/codefirst/babelfish/dto/ProjectDto.java
@@ -0,0 +1,13 @@
+package com.codefirst.babelfish.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectDto {
+
+
+    private String projectName;
+
+    private CustomerDto customerDto;
+
+}
